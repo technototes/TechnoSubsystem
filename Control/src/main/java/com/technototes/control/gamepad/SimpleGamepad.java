752c0034b022f4b27a8316d1928b9286bd722c32
1198159 <jeffastedman@gmail.com>
2020-10-06 19:00:01 -0700

    fix

--- Control/src/main/java/com/technototes/control/gamepad/SimpleGamepad.java
+++ Control/src/main/java/com/technototes/control/gamepad/SimpleGamepad.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 
 public class SimpleGamepad extends AbstractGamepad<GamepadButton, GamepadAxis> {
     public SimpleGamepad(Gamepad g) {
-        super(g);
+        super(g, GamepadButton.class, GamepadAxis.class);
     }
 
 }
